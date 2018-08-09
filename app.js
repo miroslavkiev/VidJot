59c669d2f7206a873f3974dff445d983ce175929
Myroslav Kravchenko <kravch@gmail.com>
2018-08-09 19:06:52 +0300

    Middle of 17th lesson

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
 const app = express();
+
+//Connect to mongoose
+mongoose.connect('mongodb://localhost:27017/vidjot-dev', {
+	useNewUrlParser: true
+})
+	.then(() => console.log(`MongoDB connected...`))
+	.catch(err => console.log(err));
+
+// Load idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 //Handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
