d5b84f34205820a826a274f84d1736aa49cea340
Myroslav Kravchenko <kravch@gmail.com>
2018-08-10 12:32:05 +0300

    21-th lesson

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 
 //Connect to mongoose
@@ -18,6 +19,10 @@ const Idea = mongoose.model('ideas');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const port = 80;
 
 app.get('/', (req, res) => {
@@ -29,6 +34,59 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
 	res.render('about');
+});
+
+app.get('/ideas', (req, res) => {
+	Idea.find({})
+		.sort({date:'desc'})
+		.then(ideas => {
+			res.render('ideas/index',{
+				ideas:ideas
+			})
+		})
+});
+
+app.post('/ideas', (req, res) => {
+	let errors = [];
+	if (!req.body.title){
+		errors.push({text: 'The title must be defined'});
+	}
+	if (!req.body.details){
+		errors.push({text: 'Please add some details'});
+	}
+	if (errors.length > 0){
+		res.render('ideas/add',{
+			errors: errors,
+			title: req.body.title,
+			details: req.body.details
+		})
+	} else {
+		const newUser = {
+			title: req.body.title,
+			details: req.body.details
+		};
+		new Idea(newUser)
+			.save()
+			.then(idea => {
+				res.redirect('/ideas');
+			})
+	}
+});
+
+app.get('/ideas/edit/:id', (req, res) => {
+	Idea.findOne({
+		_id: req.params.id
+	})
+		.then(idea => {
+			res.render('ideas/edit', {
+				idea: idea
+			});
+		});
+
+});
+
+app.get('/ideas/add', (req, res) => {
+	res.render('ideas/add');
 });
 
 app.listen(port, () => {
