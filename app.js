e02e61f610242121e626b614b14c510f556545ed
Myroslav Kravchenko <kravch@gmail.com>
2018-08-10 17:25:06 +0300

    EOF 24-th lesson

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 const app = express();
 
 //Connect to mongoose
@@ -20,15 +23,35 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // bodyParser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Method override middleware
+app.use(methodOverride('_method'));
+
+//express-session middleware
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+//Connect flash middleware
+app.use(flash());
+
+//Global variables
+app.use(function(req,res,next){
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
 const port = 80;
 
 app.get('/', (req, res) => {
 	const title = 'Welcome!';
-  res.render('index',{
-  	title: title
+	res.render('index', {
+		title: title
 	});
 });
 
@@ -38,24 +61,28 @@ app.get('/about', (req, res) => {
 
 app.get('/ideas', (req, res) => {
 	Idea.find({})
-		.sort({date:'desc'})
+		.sort({date: 'desc'})
 		.then(ideas => {
-			res.render('ideas/index',{
-				ideas:ideas
+			res.render('ideas/index', {
+				ideas: ideas
 			})
 		})
 });
 
+app.get('/ideas/add',(req, res) => {
+	res.render('ideas/add');
+});
+
 app.post('/ideas', (req, res) => {
 	let errors = [];
-	if (!req.body.title){
+	if (!req.body.title) {
 		errors.push({text: 'The title must be defined'});
 	}
-	if (!req.body.details){
+	if (!req.body.details) {
 		errors.push({text: 'Please add some details'});
 	}
-	if (errors.length > 0){
-		res.render('ideas/add',{
+	if (errors.length > 0) {
+		res.render('ideas/add', {
 			errors: errors,
 			title: req.body.title,
 			details: req.body.details
@@ -68,6 +95,7 @@ app.post('/ideas', (req, res) => {
 		new Idea(newUser)
 			.save()
 			.then(idea => {
+				req.flash('success_msg','The idea was successfully added!');
 				res.redirect('/ideas');
 			})
 	}
@@ -83,12 +111,31 @@ app.get('/ideas/edit/:id', (req, res) => {
 			});
 		});
 
+	app.get('/ideas/add', (req, res) => {
+		res.render('ideas/add');
+	});
 });
 
-app.get('/ideas/add', (req, res) => {
-	res.render('ideas/add');
+
+app.put('/ideas/:id', (req, res) => {
+	Idea.findByIdAndUpdate(req.params.id, {
+		title: req.body.title,
+	  details: req.body.details
+	})
+		.then(idea => {
+			req.flash('success_msg','The idea was successfully updated!');
+			res.redirect('/ideas');
+		})
+});
+
+app.delete('/ideas/:id', (req, res) => {
+	Idea.remove({_id: req.params.id})
+		.then(() => {
+			req.flash('success_msg','The idea was successfully deleted!');
+			res.redirect('/ideas');
+		})
 });
 
 app.listen(port, () => {
-  console.log(`The app started on ${port}`);
+	console.log(`The app started on ${port}`);
 });
